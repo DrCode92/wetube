3554acb5e0d42296eb65435c9ce571203e1fd0f3
DrCode92 <dr.code92@gmail.com>
2021-03-18 21:19:01 +0900

    20210318

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+// function handleListening() {
+//     console.log(`Listening on: http://localhost:${PORT}`);
+// }
+const handleListening = () => 
     console.log(`Listening on: http://localhost:${PORT}`);
-}
 
-function handleHome(req, res) {
-    console.log(req);
-    res.send("Hello from Home!")
-}
+// function handleHome(req, res) {
+//     console.log(req);
+//     res.send("Hello from Home!")
+// }
+const handleHome = (req, res) => 
+    res.send("Hello from home");
 
-function handleProfile(req, res) {
+// function handleProfile(req, res) {
+//     res.send("You are on my profile");
+// }
+const handleProfile = (req, res) => 
     res.send("You are on my profile");
-}
+
+// const betweenHome = (req, res, next) => {
+//     console.log("Between");
+//     next();
+// };
+// app.use(betweenHome);
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
+// const middleware = (req, res, next) => {
+//     res.send("not happening");
+// }
+// app.get("/", middleware, handleHome);
 
 app.get("/", handleHome);
+
 
 app.get("/profile", handleProfile);
 
