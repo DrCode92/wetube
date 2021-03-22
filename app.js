946b936453cafde7e8f4455cbb31d95822a4e06c
DrCode92 <dr.code92@gmail.com>
2021-03-22 09:49:24 +0900

    20210322

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -38,11 +39,25 @@ const app = express();
 // };
 // app.use(betweenHome);
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
+
+
+// middleware 생성 option 1
+// app.use((req, res, next) => {
+    
+// })
+
+// middleware 생성 option 2
+// app.use(function(req, res, next) {
+        
+// })
+
+app.use(localsMiddleware);
 
 // const middleware = (req, res, next) => {
 //     res.send("not happening");
